167f04cc8a7c1b0b5b617790c62c96c22ffc8b43
Philipe <dev.philipe@outlook.com>
2023-07-09 15:49:54 -0300

    feat(welcome): welcome inheriting from site_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,10 +10,9 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
-  get 'bem-vindo', to: 'welcome#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'welcome#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  get 'bem-vindo', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
+
 end
