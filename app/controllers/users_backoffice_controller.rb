6921d065247be03e0a642b25f9930abe11181080
Philipe <dev.philipe@outlook.com>
2023-07-10 20:12:48 -0300

    feat(logoff user / admin): user and admin logoff created

--- app/controllers/users_backoffice_controller.rb
+++ app/controllers/users_backoffice_controller.rb
@@ -1,4 +1,8 @@
 class UsersBackofficeController < ApplicationController
   before_action :authenticate_user!
   layout 'users_backoffice'
+  def destroy
+    sign_out(current_user)
+    redirect_to root_path, notice: "Você foi desconectado com sucesso."
+  end
 end
